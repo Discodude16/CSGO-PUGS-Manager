21ef9c51396c13654e620da28ced551a4df2fa53
Discodude16 <discodude16@icloud.com>
2018-07-23 15:37:13 -0400

    Removed the bot token variable since I forgot to do so earlier.
    Whoever came trotting on to the discord and changed the bot name and such, thanks for letting me know :)

--- src/com/csgo/manager/Main.java
+++ src/com/csgo/manager/Main.java
@@ -1,5 +1,11 @@
 package com.csgo.manager;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 import javax.security.auth.login.LoginException;
 
 import net.dv8tion.jda.core.AccountType;
@@ -9,18 +15,36 @@ import net.dv8tion.jda.core.JDABuilder;
 public class Main {
 
 	public static JDA jda;
-	public static final String BOT_TOKEN = "NDcwNzI5Nzk0MzA5MzI0ODAx.DjahNg.PDtyjArTHzqnwQQxi1RJPis4Mhs";
+	public static final String BOT_TOKEN = "";
 	
-	public static void main(String[] args) {
+	public static String getToken()
+	{
+		String token = null;
+		
+		Properties props = new Properties();
+		InputStream input = null;
 		
 		try {
-			jda = new JDABuilder(AccountType.BOT).addEventListener(new BotListener()).setToken(BOT_TOKEN).buildBlocking();
+			input = new FileInputStream("config.properties");
+			props.load(input);
+			token = props.getProperty("botToken");
+		} catch (FileNotFoundException e) {
+			System.out.println("Cannot Find Properties File! Finding System Variable");
+			token = System.getenv("BOT_TOKEN");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		return token;
+	}
+	
+	public static void main(String[] args) {
+		try {
+			jda = new JDABuilder(AccountType.BOT).addEventListener(new BotListener()).setToken(getToken()).buildBlocking();
 			//BotListener.retrieveUsers();
 		} catch (LoginException | InterruptedException e) {
 			e.printStackTrace();
 		}
-		
-
 	}
 
 }
